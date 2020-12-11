7e62a9a6fa2b8c584c426f1a949205f257b38f83
kevinsunchen <kschen0520@gmail.com>
2020-12-11 04:42:33 -0800

    update

--- optical_flow.py
+++ optical_flow.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 
 # From opencv
-cap = cv.VideoCapture("1_2016-05-02_23-06-32.mp4")
+cap = cv.VideoCapture("1_2016-05-02_22-46-25.mp4")
 # read the first frame of the video
 ret, frame1 = cap.read()
 # below is used for cropping the video
@@ -10,26 +10,32 @@ ret, frame1 = cap.read()
 # frame1 = frame1[crop:, :]
 
 prvs = cv.cvtColor(frame1,cv.COLOR_BGR2GRAY)
+cv.imshow('frame2 original', prvs)
 # create an array of equal size to the frame
 hsv = np.zeros_like(frame1)
 # set all pixels to black
 hsv[...,1] = 255
+
+
 while(1):
     ret, frame2 = cap.read()
     # for cropping --> frame2 = frame2[crop:, :]
     next = cv.cvtColor(frame2,cv.COLOR_BGR2GRAY)
+    next = cv.fastNlMeansDenoising(next, None, 15, 7, 21)
+    cv.imshow('test', next)
+
     # utilizing Gunner Farneback's algorithm -- need to read more about how this works
-    flow = cv.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    flow = cv.calcOpticalFlowFarneback(prev=prvs, next=next, flow=None, pyr_scale=0.5, levels=3, winsize=30, iterations=3, poly_n=5, poly_sigma=1.2, flags=0)
     # direction corresponds to Hue value of image, magnitude corresponds to Value plane
     mag, ang = cv.cartToPolar(flow[...,0], flow[...,1])
     hsv[...,0] = ang*180/np.pi/2
     # the following method of thresholding is pretty inefficient
     thresh = cv.normalize(mag,None,0,255,cv.NORM_MINMAX)
-    y = []
-    for i in thresh:
-        y.append([j if j > 120 else 0 for j in i ])
+    #a = cv.adaptiveThreshold(np.uint8(thresh), 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
+    ret, y = cv.threshold(thresh,110,255,cv.THRESH_TOZERO)
     hsv[...,2] = y
-    # hsv[...,2] = thresh
+    #print(np.array(y))
     bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
     cv.imshow('frame2 original', frame2)
     cv.imshow('frame2',bgr)
